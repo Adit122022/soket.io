34654eae24f95863c85b472b1f1ca1a52c6bdfef
Adit122022 <adity202212@gmail.com>
2025-02-21 12:16:16 +0530

    .......

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/' , (req,res) =>{
-    res.send('Hello World');
+    res.send('AFRICA KI MUDRAA');
 })
 
 module.exports = app;
