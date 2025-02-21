1378705f4de74a34b0c4d47096b17873f437de6d
Adit122022 <adity202212@gmail.com>
2025-02-21 12:21:36 +0530

    geanerating socket.on('message', message => {         console.log('Received message:', message);         io.emit('message', message);  // broadcast the message to all connected clients     });  its an event

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const server = require('http').createServer(app);  // pass the app
 //  socket.io setup for real-time communication  // copy the code from the npm soket.io package ffrom the npm webstie
 const io = require('socket.io')(server);
 io.on('connection', socket => {
+    socket.on('message', message => {
+        console.log('Received message:', message);
+        io.emit('message', message);  // broadcast the message to all connected clients
+    });
     console.log('New user connected');  // a new user connected to the server
 });
 server.listen(3000 ,() => {
