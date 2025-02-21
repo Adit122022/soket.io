50de658892295c0756eb4a658c5138fc6382ad38
Adit122022 <adity202212@gmail.com>
2025-02-21 11:48:56 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+
+const app = require('./src/app');
+//  read documentation
+const server = require('http').createServer(app);  // pass the app 
+
+//  socket.io setup for real-time communication  // copy the code from the npm soket.io package ffrom the npm webstie
+const io = require('socket.io')(server);
+io.on('connection', socket => {
+    console.log('New user connected');  // a new user connected to the server
+});
+server.listen(3000 ,() => {
+    console.log('Server is running on port 3000');  // Server is listening on port 3000
+});
+
